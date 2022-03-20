9308492fd7252f24ddac29ce3bd2d1c971e12833
katerinavr <katerina_vriza@yahoo.ge>
2022-03-20 16:22:50 +0000

    update

--- deep_one_class/gnn.py
+++ deep_one_class/gnn.py
@@ -107,8 +107,8 @@ def gnn_score_dropout(smiles1, smiles2):
    
     validation_set= get_representation(smiles1, smiles2)    
     smiles2txt(validation_set)
-    subprocess.call("python gnn/main.py -fi gnn/smiles1.txt -m gin_supervised_masking -o gnn/results1", shell=True)
-    subprocess.call("python gnn/main.py -fi gnn/smiles2.txt -m gin_supervised_masking -o gnn/results2", shell=True)
+    subprocess.call("python3 gnn/main.py -fi gnn/smiles1.txt -m gin_supervised_masking -o gnn/results1", shell=True)
+    subprocess.call("python3 gnn/main.py -fi gnn/smiles2.txt -m gin_supervised_masking -o gnn/results2", shell=True)
     valid1 = np.load('gnn/results1/mol_emb.npy')
     valid2 = np.load('gnn/results2/mol_emb.npy')
     #print(valid1.shape)
